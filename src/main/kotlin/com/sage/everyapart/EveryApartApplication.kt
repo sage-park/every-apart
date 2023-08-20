7809dd51632045c57997ebff6ec3b73e7bfa21b2
Sage-Park <sagepark94@gmail.com>
2023-08-20 16:31:39 +0900

    first commit

---
+++ src/main/kotlin/com/sage/everyapart/EveryApartApplication.kt
@@ -0,0 +1,11 @@
+package com.sage.everyapart
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class EveryApartApplication
+
+fun main(args: Array<String>) {
+    runApplication<EveryApartApplication>(*args)
+}
---
+++ src/test/kotlin/com/sage/everyapart/EveryApartApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.sage.everyapart
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class EveryApartApplicationTests {
+
+    @Test
+    fun contextLoads() {
+    }
+
+}
